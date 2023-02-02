09170f9bd5b0506078bdac0171ec83b50ea5ac26
Augusto Romero <augustoromerodev@gmail.com>
2023-02-02 19:34:58 -0300

    Config OPEN AI API

---
+++ src/config/index.ts
@@ -0,0 +1,16 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export async function getResponse() {
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: "Say this is a test",
+    temperature: 0,
+    max_tokens: 7,
+  });
+  return response;
+}
